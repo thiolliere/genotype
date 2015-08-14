3605331886856d0225b315248e798f0f74ae5e67
thiolliere <guillaume.thiolliere@voila.fr>
2015-08-14 16:02:40 +0200

    serve.bash

--- serve.bash
+++ serve.bash
@@ -5,12 +5,11 @@ then
        	sudo tc qdisc add dev lo root netem delay 50ms 5ms distribution normal
 fi
 
-love source server > slog &
-love source > clog 
+love source server 
 
 if [ "$1" = "delay" ] 
 then
 	sudo tc qdisc del dev lo root
 fi
-sleep 1
-pkill -9 love
+#sleep 1
+#pkill -9 love
